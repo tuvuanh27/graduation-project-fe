dfa8c1f58c7a1fe422acc284cd5b7c0a4407670f
tuvuanh27 <vuanhtu111127@gmailcom>
2023-03-05 01:11:52 +0700

    add github ci

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/gareys-cb/example-dapp.git master:gh-pages
+git push -f https://github.com/tuvuanh27/graduation-project-fe.git main:gh-pages
 
 cd -
